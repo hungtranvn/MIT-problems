b3684e2b3fec8f062858b4bef05200c1ed9cf970
HungTran <32216529+hungtranvn@users.noreply.github.com>
2019-11-09 10:32:13 +0700

    Update ps3.py

--- ps3.py
+++ ps3.py
@@ -16,7 +16,7 @@ CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 10
 
 SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+    '*':0 'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
 
 # -----------------------------------
@@ -217,7 +217,7 @@ def is_valid_word(word, hand, word_list):
     #pass  # TO DO... Remove this line when you implement this function
     
     word = word.lower()
-    word_dict = def get_frequency_dict(word):
+    word_dict = get_frequency_dict(word)
 
     for char in word_dict.keys():
         if char not in hand.keys():
