dbe611a4d78b971b89b12ae09a29aa742fa44b10
HungTran <32216529+hungtranvn@users.noreply.github.com>
2019-12-16 12:20:54 +0700

    Update ps3.py

--- ps3.py
+++ ps3.py
@@ -3,7 +3,7 @@
 # The 6.0001 Word Game
 # Created by: Kevin Luu <luuk> and Jenna Wiens <jwiens>
 #
-# Name          : <your name>
+# Name          : HungTran and others
 # Collaborators : <your collaborators>
 # Time spent    : <total time>
 
